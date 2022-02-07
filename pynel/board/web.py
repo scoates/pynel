91ba53988289a42e65bb706c9ba01b43367087b6
Sean Coates <sean@seancoates.com>
2022-02-07 07:25:13 -0500

    rework templates to push the 'heavy'  work onto the browser; reallocations/string manipulation are expensive on the ESP32's PSRAM

--- pynel/board/web.py
+++ pynel/board/web.py
@@ -2,27 +2,40 @@ from microdot import Microdot, Response
 from context import Context
 from img import read_image, write_image, show_image
 from palette import PALETTE
-from views.home import view as view_home
-from views.img import view as view_img
+from util import get_dir
 
 app = Microdot()
 
 
 @app.get("/")
 def home(request):
+    with open(get_dir(__file__) + "/views/home.html") as f:
+        return Response(
+            f.read(),
+            200,
+            {"Content-Type": "text/html"},
+        )
+
+
+@app.get("/config")
+def config(request):
     return Response(
-        view_home(
-            pixels=read_image(),
-            scale=10,
-            palette=PALETTE,
-            view_img=view_img,
-            w=Context.data["CONFIG"]["PIXELS_W"],
-            h=Context.data["CONFIG"]["PIXELS_H"],
-            r=2,
-        ),
-        200,
-        {"Content-Type": "text/html"},
+        {
+            "scale": 10,
+            "w": Context.data["CONFIG"]["PIXELS_W"],
+            "h": Context.data["CONFIG"]["PIXELS_H"],
+        }
     )
+
+
+@app.get("/pixels")
+def pixels(request):
+    return Response(read_image())
+
+
+@app.get("/palette")
+def palette(request):
+    return Response(PALETTE)
 
 
 @app.post("/change")
